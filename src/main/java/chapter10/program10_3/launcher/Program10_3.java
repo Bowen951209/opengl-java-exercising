e721045e30c87e842a8fe660223eca4a4a630b57
bowen51209 <bowen51209@gmail.com>
2023-06-17 18:28:50 +0800

    copy a brand new Program10_3.

---
+++ src/main/java/chapter10/program10_3/launcher/Program10_3.java
@@ -0,0 +1,14 @@
+package chapter10.program10_3.launcher;
+
+import chapter10.program10_2.launcher.Program10_2;
+
+// Height Mapping
+public class Program10_3 extends Program10_2 {
+    public Program10_3(String title) {
+        super(title);
+    }
+
+    public static void main(String[] args) {
+        new Program10_3("Height Mapping");
+    }
+}
