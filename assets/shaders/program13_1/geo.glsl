c12c4142191cc7128d2ef060170ee0293e76e25a
bowen51209 <bowen51209@gmail.com>
2023-07-12 08:13:26 +0800

    Edit geo.glsl to Figure 13.3.

--- assets/shaders/program13_1/geo.glsl
+++ assets/shaders/program13_1/geo.glsl
@@ -36,10 +36,13 @@ uniform mat4 norm_matrix;
 uniform float inflateValue; // 膨脹
 
 void main(void) {
+     // A triangle's normal = average of 3 vertices' normals.
+     vec4 triangleNormal = vec4((varyingNormal[0] + varyingNormal[1] + varyingNormal[2]) / 3.0, 1.0);
+
      // Move vertices along the normal
      for(int i = 0; i < 3; i++) {
           // in point -> move -> view space
-          vec4 movedPoint = gl_in[i].gl_Position + normalize(vec4(varyingNormal[i], 1.0)) * inflateValue;
+          vec4 movedPoint = gl_in[i].gl_Position + normalize(triangleNormal) * inflateValue;
           gl_Position = p_matrix * v_matrix * movedPoint;
 
           varyingNormalG = varyingNormal[i];
