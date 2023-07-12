3aaf56d1ee7c0d32492e2cd801f870e34205b834
bowen51209 <bowen51209@gmail.com>
2023-07-12 18:39:38 +0800

    added Program13_4

---
+++ assets/shaders/program13_4/geo.glsl
@@ -0,0 +1,70 @@
+#version 430
+
+// Inputs from vertex shader
+layout (triangles) in;
+in vec3 varyingNormal[];
+in vec3 varyingLightDir[];
+in vec3 varyingHalfVector[];
+
+// Outputs through rasterizer to fragment shader.
+layout (line_strip, max_vertices = 2) out;
+out vec3 varyingNormalG;
+out vec3 varyingLightDirG;
+out vec3 varyingHalfVectorG;
+out vec3 varyingVertPosrG;
+
+struct PositionalLight
+{ vec4 ambient;
+    vec4 diffuse;
+    vec4 specular;
+    vec3 position;
+};
+
+struct Material
+{ vec4 ambient;
+    vec4 diffuse;
+    vec4 specular;
+    float shininess;
+};
+
+uniform vec4 globalAmbient;
+uniform PositionalLight light;
+uniform Material material;
+uniform mat4 mv_matrix;
+uniform mat4 proj_matrix;
+uniform mat4 norm_matrix;
+
+float lineHeight = 0.5f;
+
+void main(void) {
+    // A triangle's normal = average of 3 vertices' normals.
+    vec4 triangleNormal = vec4((varyingNormal[0] + varyingNormal[1] + varyingNormal[2]) / 3.0, 1.0);
+
+    // oringinal point
+    vec3 oringinalPoint0 = gl_in[0].gl_Position.xyz;
+    vec3 oringinalPoint1 = gl_in[1].gl_Position.xyz;
+    vec3 oringinalPoint2 = gl_in[2].gl_Position.xyz;
+
+    vec3 movedPoint0 = gl_in[0].gl_Position.xyz + normalize(varyingNormal[0]) * lineHeight;
+    vec3 movedPoint1 = gl_in[1].gl_Position.xyz + normalize(varyingNormal[1]) * lineHeight;
+    vec3 movedPoint2 = gl_in[2].gl_Position.xyz + normalize(varyingNormal[2]) * lineHeight;
+
+    vec3 startPoint = (oringinalPoint0 + oringinalPoint1 + oringinalPoint2) / 3.0;
+    vec3 endPoint = (movedPoint0 + movedPoint1 + movedPoint2) / 3.0;
+
+    // first vertex of the line primitive
+    gl_Position = proj_matrix * mv_matrix * vec4(startPoint, 1.0);
+    varyingVertPosrG = startPoint;
+    varyingLightDirG = light.position - startPoint;
+    varyingNormalG = varyingNormal[0];
+    EmitVertex();
+
+    // second vertex of the line primitive
+    gl_Position = proj_matrix * mv_matrix * vec4(endPoint, 1.0);
+    varyingVertPosrG = endPoint;
+    varyingLightDirG = light.position - endPoint;
+    varyingNormalG = varyingNormal[1];
+    EmitVertex();
+
+    EndPrimitive();
+}
---
+++ src/main/java/chapter13/program13_4/Program13_4.java
@@ -0,0 +1,65 @@
+package chapter13.program13_4;
+
+import chapter13.program13_2.Program13_2;
+import org.joml.Vector3f;
+import utilities.GLFWWindow;
+import utilities.Materials;
+import utilities.ShaderProgram;
+import utilities.ValuesContainer;
+import utilities.exceptions.InvalidMaterialException;
+import utilities.models.Torus;
+
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL20.*;
+
+public class Program13_4 extends Program13_2 {
+    private Program13_4() {
+        super();
+    }
+
+    @Override
+    protected void init() {
+        glfwWindow = new GLFWWindow(1500, 1000, "Prog13.4 Changing Primitive type");
+
+        shaderProgram = new ShaderProgram(
+                "assets/shaders/program13_4/vert.glsl",
+                "assets/shaders/program13_4/frag.glsl",
+                "assets/shaders/program13_4/geo.glsl"
+        );
+        shaderProgram.use();
+        torus = new Torus(.5f, .2f, 48, true, new Vector3f());
+        try {
+            material = new Materials("gold");
+        } catch (InvalidMaterialException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void drawScene() {
+        torus.updateState(camera);
+        glUniform4fv(shaderProgram.getUniformLoc("globalAmbient"), light.getGlobalAmbient());
+        glUniform4fv(shaderProgram.getUniformLoc("light.ambient"), light.getLightAmbient());
+        glUniform4fv(shaderProgram.getUniformLoc("light.specular"), light.getLightDiffuse());
+        glUniform4fv(shaderProgram.getUniformLoc("light.diffuse"), light.getLightSpecular());
+        glUniform3fv(shaderProgram.getUniformLoc("light.position"), light.getLightPosition());
+        light.flipAll();
+
+        glUniform4fv(shaderProgram.getUniformLoc("material.ambient"), material.getAmbient());
+        glUniform4fv(shaderProgram.getUniformLoc("material.specular"), material.getDiffuse());
+        glUniform4fv(shaderProgram.getUniformLoc("material.diffuse"), material.getSpecular());
+        glUniform1fv(shaderProgram.getUniformLoc("material.shininess"), material.getShininess());
+        material.flipAll();
+
+        glUniformMatrix4fv(shaderProgram.getUniformLoc("mv_matrix"), false, torus.getMvMat().get(ValuesContainer.VALS_OF_16));
+        glUniformMatrix4fv(shaderProgram.getUniformLoc("proj_matrix"), false, camera.getProjMat().get(ValuesContainer.VALS_OF_16));
+        glUniformMatrix4fv(shaderProgram.getUniformLoc("norm_matrix"), false, torus.getInvTrMat().get(ValuesContainer.VALS_OF_16));
+
+        glFrontFace(GL_CCW);
+        torus.draw(GL_TRIANGLES);
+    }
+
+    public static void main(String[] args) {
+        new Program13_4().run(false, false);
+    }
+}
