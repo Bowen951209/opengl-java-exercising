3c469e0b31359b73af6b7b0e6e19f795c625ab99
bowen51209 <bowen51209@gmail.com>
2023-07-19 20:30:51 +0800

    Add noise pattern.

--- src/main/java/engine/sceneComponents/Texture3D.java
+++ src/main/java/engine/sceneComponents/Texture3D.java
@@ -1,5 +1,6 @@
 package engine.sceneComponents;
 
+import engine.exceptions.InvalidPatternException;
 import engine.util.Timer;
 import org.lwjgl.BufferUtils;
 
@@ -13,16 +14,28 @@ public class Texture3D extends Thread {
     private final int textureWidth = 256;
     private final int textureHeight = 256;
     private final int textureDepth = 256;
+    private int zoom = 1;
+
+    public void setZoom(int zoom) {
+        this.zoom = zoom;
+    }
+
     private final float[][][] texture3DPattern = new float[textureWidth][textureHeight][textureDepth];
     private ByteBuffer data;
+    private final String pattern;
 
-    public Texture3D(int usingUnit) {
+    public Texture3D(int usingUnit, String pattern) {
         this.usingUnit = usingUnit;
+        this.pattern = pattern;
     }
 
     @Override
     public void run() {
-        generatePattern();
+        switch (pattern.toUpperCase()) {
+            case "STRIPE" -> generatePattern();
+            case "NOISE" -> generateNoise();
+            default -> throw new InvalidPatternException("Unsupported pattern passed in");
+        }
         fillDataArray(); // This take quite long.
     }
 
@@ -49,6 +62,16 @@ public class Texture3D extends Thread {
         }
     }
 
+    private void generateNoise() {
+        for (int x = 0; x < textureWidth; x++) {
+            for (int y = 0; y < textureHeight; y++) {
+                for (int z = 0; z < textureDepth; z++) {
+                    texture3DPattern[x][y][z] = (float) Math.random();
+                }
+            }
+        }
+    }
+
     private void fillDataArray() {
         Timer timer = new Timer();
         timer.start();
@@ -57,24 +80,40 @@ public class Texture3D extends Thread {
         for (int x = 0; x < textureWidth; x++) {
             for (int y = 0; y < textureHeight; y++) {
                 for (int z = 0; z < textureDepth; z++) {
-                    if (texture3DPattern[z][y][z] == 1.0) {
-                        // yellow
-                        data.put((byte) 255); // r
-                        data.put((byte) 255); // g
-                        data.put((byte) 0);// b
-                        data.put((byte) 255); // a
-                    } else {
-                        // blue
-                        data.put((byte) 0); // r
-                        data.put((byte) 0); // g
-                        data.put((byte) 255);// b
-                        data.put((byte) 255); // a
+                    switch (pattern.toUpperCase()) {
+                        case "STRIPE" -> fillStripe(x, y, z);
+                        case "NOISE" -> fillNoise(x, y, z);
                     }
                 }
             }
         }
         data.flip();
         timer.end("Fill data in array for 3D texture takes: ");
+    }
+
+    private void fillStripe(int x, int y, int z) {
+        if (texture3DPattern[x][y][z] == 1.0) {
+            // yellow
+            data.put((byte) 255); // r
+            data.put((byte) 255); // g
+            data.put((byte) 0);// b
+            data.put((byte) 255); // a
+        } else {
+            // blue
+            data.put((byte) 0); // r
+            data.put((byte) 0); // g
+            data.put((byte) 255);// b
+            data.put((byte) 255); // a
+        }
+    }
+
+    public void fillNoise(int x, int y, int z) {
+        float mappedValue = texture3DPattern[x / zoom][y / zoom][z / zoom];
+
+        data.put((byte) (mappedValue * 255)); // r
+        data.put((byte) (mappedValue * 255)); // g
+        data.put((byte) (mappedValue * 255));// b
+        data.put((byte) 255); // a
     }
 
     private void loadToTexture() {
