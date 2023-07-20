0294fc9e7bc80623cae56e40fe523b8d734b135d
bowen51209 <bowen51209@gmail.com>
2023-07-20 08:10:45 +0800

    add smooth noise

--- src/main/java/engine/sceneComponents/Texture3D.java
+++ src/main/java/engine/sceneComponents/Texture3D.java
@@ -32,8 +32,8 @@ public class Texture3D extends Thread {
     @Override
     public void run() {
         switch (pattern.toUpperCase()) {
-            case "STRIPE" -> generatePattern();
-            case "NOISE" -> generateNoise();
+            case "STRIPE" -> generateStripe();
+            case "NOISE", "SMOOTH" -> generateNoise();
             default -> throw new InvalidPatternException("Unsupported pattern passed in");
         }
         fillDataArray(); // This take quite long.
@@ -49,7 +49,7 @@ public class Texture3D extends Thread {
         loadToTexture();
     }
 
-    private void generatePattern() {
+    private void generateStripe() {
         for (int x = 0; x < textureWidth; x++) {
             for (int y = 0; y < textureHeight; y++) {
                 for (int z = 0; z < textureDepth; z++) {
@@ -72,6 +72,37 @@ public class Texture3D extends Thread {
         }
     }
 
+    public double smoothNoise(double x1, double y1, double z1) {
+        // TODO: 2023/7/20 go to float
+        //get fractional part of x, y, and z
+        double fractX = x1 - (int) x1;
+        double fractY = y1 - (int) y1;
+        double fractZ = z1 - (int) z1;
+
+        //neighbor values
+        double x2 = x1 - 1;
+        if (x2 < 0) x2 = Math.round(textureWidth / (float)zoom) - 1.0;
+        double y2 = y1 - 1;
+        if (y2 < 0) y2 = Math.round(textureHeight / (float)zoom) - 1.0;
+        double z2 = z1 - 1;
+        if (z2 < 0) z2 = Math.round(textureDepth / (float)zoom) - 1.0;
+
+        //smooth the noise by interpolating
+        double value = 0.0;
+        value += fractX * fractY * fractZ * texture3DPattern[(int) x1][(int) y1][(int) z1];
+        value += (1.0 - fractX) * fractY * fractZ * texture3DPattern[(int) x2][(int) y1][(int) z1];
+        value += fractX * (1.0 - fractY) * fractZ * texture3DPattern[(int) x1][(int) y2][(int) z1];
+        value += (1.0 - fractX) * (1.0 - fractY) * fractZ * texture3DPattern[(int) x2][(int) y2][(int) z1];
+
+        value += fractX * fractY * (1.0 - fractZ) * texture3DPattern[(int) x1][(int) y1][(int) z2];
+        value += (1.0 - fractX) * fractY * (1.0 - fractZ) * texture3DPattern[(int) x2][(int) y1][(int) z2];
+        value += fractX * (1.0 - fractY) * (1.0 - fractZ) * texture3DPattern[(int) x1][(int) y2][(int) z2];
+        value += (1.0 - fractX) * (1.0 - fractY) * (1.0 - fractZ) * texture3DPattern[(int) x2][(int) y2][(int) z2];
+
+        return value;
+    }
+
+    // TODO: 2023/7/20 convert to static
     private void fillDataArray() {
         Timer timer = new Timer();
         timer.start();
@@ -83,6 +114,7 @@ public class Texture3D extends Thread {
                     switch (pattern.toUpperCase()) {
                         case "STRIPE" -> fillStripe(x, y, z);
                         case "NOISE" -> fillNoise(x, y, z);
+                        case "SMOOTH" -> fillSmoothNoise(x, y, z);
                     }
                 }
             }
@@ -109,6 +141,16 @@ public class Texture3D extends Thread {
 
     public void fillNoise(int x, int y, int z) {
         float mappedValue = texture3DPattern[x / zoom][y / zoom][z / zoom];
+
+        data.put((byte) (mappedValue * 255)); // r
+        data.put((byte) (mappedValue * 255)); // g
+        data.put((byte) (mappedValue * 255));// b
+        data.put((byte) 255); // a
+    }
+
+    private void fillSmoothNoise(double x, double y, double z) {
+        // TODO: 2023/7/20 go to float
+        double mappedValue = smoothNoise(x / zoom, y / zoom, z / zoom);
 
         data.put((byte) (mappedValue * 255)); // r
         data.put((byte) (mappedValue * 255)); // g
