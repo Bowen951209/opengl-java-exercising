7ca997f8bd9d5e5bfc3c33448d68213716b1717a
bowen51209 <bowen51209@gmail.com>
2023-07-22 08:13:41 +0800

    1.Uses @alksily NoiseGenerator class in my Texture3D class; 2.Tidy up stripe generator method and combine to single method.

--- src/main/java/engine/sceneComponents/Texture3D.java
+++ src/main/java/engine/sceneComponents/Texture3D.java
@@ -1,6 +1,7 @@
 package engine.sceneComponents;
 
 import engine.exceptions.InvalidPatternException;
+import engine.util.NoiseGenerator;
 import engine.util.Timer;
 import org.lwjgl.BufferUtils;
 
@@ -20,9 +21,10 @@ public class Texture3D extends Thread {
         this.zoom = zoom;
     }
 
-    private final float[][][] texture3DPattern = new float[textureWidth][textureHeight][textureDepth];
     private ByteBuffer data;
     private final String pattern;
+
+    private final NoiseGenerator noiseGenerator = new NoiseGenerator();  // This would be random seed.
 
     public Texture3D(int usingUnit, String pattern) {
         this.usingUnit = usingUnit;
@@ -31,11 +33,6 @@ public class Texture3D extends Thread {
 
     @Override
     public void run() {
-        switch (pattern.toUpperCase()) {
-            case "STRIPE" -> generateStripe();
-            case "NOISE", "SMOOTH", "MIX-SMOOTH" -> generateNoise();
-            default -> throw new InvalidPatternException("Unsupported pattern passed in");
-        }
         fillDataArray(); // This take quite long.
     }
 
@@ -49,33 +46,6 @@ public class Texture3D extends Thread {
         loadToTexture();
     }
 
-
-    // -------------------These generate methods generate data to pattern----------------------------------
-    private void generateStripe() {
-        for (int x = 0; x < textureWidth; x++) {
-            for (int y = 0; y < textureHeight; y++) {
-                for (int z = 0; z < textureDepth; z++) {
-                    if ((y / 10) % 2 == 0)
-                        texture3DPattern[x][y][z] = 0.0f;
-                    else
-                        texture3DPattern[x][y][z] = 1.0f;
-                }
-            }
-        }
-    }
-
-    private void generateNoise() {
-        for (int x = 0; x < textureWidth; x++) {
-            for (int y = 0; y < textureHeight; y++) {
-                for (int z = 0; z < textureDepth; z++) {
-                    texture3DPattern[x][y][z] = (float) Math.random();
-                }
-            }
-        }
-    }
-    // -----------------------------------------------------------------------------------------------------
-
-
     // TODO: 2023/7/20 convert to static
     private void fillDataArray() {
         Timer timer = new Timer();
@@ -86,10 +56,10 @@ public class Texture3D extends Thread {
             for (int y = 0; y < textureHeight; y++) {
                 for (int z = 0; z < textureDepth; z++) {
                     switch (pattern.toUpperCase()) {
-                        case "STRIPE" -> fillStripe(x, y, z);
-                        case "NOISE" -> fillNoise(x, y, z);
+                        case "STRIPE" -> fillStripe(y);
                         case "SMOOTH" -> fillSmoothNoise(x, y, z);
-                        case "MIX-SMOOTH" -> fillMixedLevelsSmoothNoise(x, y, z);
+                        case "MIX-SMOOTH" -> fillSmoothNoise(x, y, z, zoom); // With levels mixed
+                        default -> throw new InvalidPatternException();
                     }
                 }
             }
@@ -98,10 +68,8 @@ public class Texture3D extends Thread {
         timer.end("\"" + pattern + "\" fills data in array takes: ");
     }
 
-
-    // --------------Convert texture3DPattern to 1D buffer---------------
-    private void fillStripe(int x, int y, int z) {
-        if (texture3DPattern[x][y][z] == 1.0) {
+    private void fillStripe(int y) {
+        if ((y / 10) % 2 == 0) {
             // yellow
             data.put((byte) 255); // r
             data.put((byte) 255); // g
@@ -116,8 +84,9 @@ public class Texture3D extends Thread {
         }
     }
 
-    private void fillNoise(int x, int y, int z) {
-        float mappedValue = texture3DPattern[x / zoom][y / zoom][z / zoom];
+
+    private void fillSmoothNoise(float x, float y, float z, int zoom) {
+        float mappedValue = (float) noiseGenerator.noise(x, y, z, zoom);
 
         data.put((byte) (mappedValue * 255)); // r
         data.put((byte) (mappedValue * 255)); // g
@@ -126,7 +95,7 @@ public class Texture3D extends Thread {
     }
 
     private void fillSmoothNoise(float x, float y, float z) {
-        float mappedValue = smoothNoise(x / zoom, y / zoom, z / zoom);
+        float mappedValue = (float) noiseGenerator.noise(x / zoom, y / zoom, z / zoom, 1);
 
         data.put((byte) (mappedValue * 255)); // r
         data.put((byte) (mappedValue * 255)); // g
@@ -134,56 +103,6 @@ public class Texture3D extends Thread {
         data.put((byte) 255); // a
     }
 
-    private void fillMixedLevelsSmoothNoise(float x, float y, float z) {
-        int maxZoom = 32;
-        float mappedValue = mixedZoomLevels(x, y, z, maxZoom);
-
-        data.put((byte) (mappedValue * 255)); // r
-        data.put((byte) (mappedValue * 255)); // g
-        data.put((byte) (mappedValue * 255));// b
-        data.put((byte) 255); // a
-    }
-
-    // -----------------This will use the texture3DPattern and calculate to smooth pattern------------------
-    private float smoothNoise(float x1, float y1, float z1) {
-        //get fractional part of x, y, and z
-        float fractX = x1 - (int) x1;
-        float fractY = y1 - (int) y1;
-        float fractZ = z1 - (int) z1;
-
-        //neighbor values
-        float x2 = x1 - 1;
-        if (x2 < 0) x2 = (float) (Math.round(textureWidth / (float) zoom) - 1.0);
-        float y2 = y1 - 1;
-        if (y2 < 0) y2 = (float) (Math.round(textureHeight / (float) zoom) - 1.0);
-        float z2 = z1 - 1;
-        if (z2 < 0) z2 = (float) (Math.round(textureDepth / (float) zoom) - 1.0);
-
-        //smooth the noise by interpolating
-        float value = 0.0f;
-        value += fractX * fractY * fractZ * texture3DPattern[(int) x1][(int) y1][(int) z1];
-        value += (1.0 - fractX) * fractY * fractZ * texture3DPattern[(int) x2][(int) y1][(int) z1];
-        value += fractX * (1.0 - fractY) * fractZ * texture3DPattern[(int) x1][(int) y2][(int) z1];
-        value += (1.0 - fractX) * (1.0 - fractY) * fractZ * texture3DPattern[(int) x2][(int) y2][(int) z1];
-
-        value += fractX * fractY * (1.0 - fractZ) * texture3DPattern[(int) x1][(int) y1][(int) z2];
-        value += (1.0 - fractX) * fractY * (1.0 - fractZ) * texture3DPattern[(int) x2][(int) y1][(int) z2];
-        value += fractX * (1.0 - fractY) * (1.0 - fractZ) * texture3DPattern[(int) x1][(int) y2][(int) z2];
-        value += (1.0 - fractX) * (1.0 - fractY) * (1.0 - fractZ) * texture3DPattern[(int) x2][(int) y2][(int) z2];
-
-        return value;
-    }
-
-    private float mixedZoomLevels(float x, float y, float z, float maxZoom) {
-        float sum = 0f;
-        float zoom = maxZoom;
-        for (float i = maxZoom; i > this.zoom; i /= 2) {
-            sum += smoothNoise(x / zoom, y / zoom, z / zoom) ;
-            zoom /= 2f;
-        }
-        sum = 128f * sum / maxZoom; // make sure sum < 256 when maxZoom >= 64
-        return sum;
-    }
 
     private void loadToTexture() {
         textureID = glGenTextures();
