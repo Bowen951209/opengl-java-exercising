20962d533707d2b47ba31df09d2e028bbdf6620d
bowen51209 <bowen51209@gmail.com>
2023-07-22 08:09:20 +0800

    add no argument constructor for InvalidPatternException

--- src/main/java/engine/exceptions/InvalidPatternException.java
+++ src/main/java/engine/exceptions/InvalidPatternException.java
@@ -1,6 +1,7 @@
 package engine.exceptions;
 
 public class InvalidPatternException extends RuntimeException {
+    public InvalidPatternException() {super();}
     public InvalidPatternException(String msg) {
         super(msg);
     }
