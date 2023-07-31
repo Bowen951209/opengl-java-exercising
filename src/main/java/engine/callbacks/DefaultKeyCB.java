0b0b4cf01fec9738404d855322d021e5de77902d
bowen51209 <bowen51209@gmail.com>
2023-07-31 21:04:59 +0800

    Update DefaultKeyCB's adding callbacks method to using HashMap. It's easier to add new callbacks.

--- src/main/java/engine/callbacks/DefaultKeyCB.java
+++ src/main/java/engine/callbacks/DefaultKeyCB.java
@@ -1,41 +1,40 @@
 package engine.callbacks;
 
-import org.lwjgl.glfw.GLFWKeyCallback;
 import engine.sceneComponents.Camera;
+import org.lwjgl.glfw.GLFWKeyCallback;
+
+import java.util.HashMap;
 
 import static org.lwjgl.glfw.GLFW.*;
 
 class DefaultKeyCB extends GLFWKeyCallback {
-    private final DefaultCursorCB cursorCB;
-    private final Camera camera;
+    private final HashMap<Integer, Runnable> pressKeymap = new HashMap<>(), releaseKeymap = new HashMap<>();
 
     public DefaultKeyCB(Camera camera, DefaultCursorCB cursorCB) {
-        this.camera = camera;
-        this.cursorCB = cursorCB;
+        pressKeymap.put(GLFW_KEY_W, camera::forward);
+        releaseKeymap.put(GLFW_KEY_W, camera::cancelF);
+        pressKeymap.put(GLFW_KEY_S, camera::backward);
+        releaseKeymap.put(GLFW_KEY_S, camera::cancelB);
+        pressKeymap.put(GLFW_KEY_A, camera::left);
+        releaseKeymap.put(GLFW_KEY_A, camera::cancelL);
+        pressKeymap.put(GLFW_KEY_D, camera::right);
+        releaseKeymap.put(GLFW_KEY_D, camera::cancelR);
+        pressKeymap.put(GLFW_KEY_SPACE, camera::fly);
+        releaseKeymap.put(GLFW_KEY_SPACE, camera::cancelFly);
+        pressKeymap.put(GLFW_KEY_LEFT_SHIFT, camera::land);
+        releaseKeymap.put(GLFW_KEY_LEFT_SHIFT, camera::cancelLand);
+
+        pressKeymap.put(GLFW_KEY_F1, cursorCB::changeLock);
     }
 
     @Override
     public void invoke(long window, int key, int scancode, int action, int mods) {
         if (action == GLFW_PRESS) {
-            switch (key) {
-                case GLFW_KEY_W -> camera.forward();
-                case GLFW_KEY_S -> camera.backward();
-                case GLFW_KEY_A -> camera.left();
-                case GLFW_KEY_D -> camera.right();
-                case GLFW_KEY_SPACE -> camera.fly();
-                case GLFW_KEY_LEFT_SHIFT -> camera.land();
-
-                case GLFW_KEY_F1 -> cursorCB.changeLock();
-            }
+            if (pressKeymap.get(key) != null) // Defined keybindings
+                pressKeymap.get(key).run();
         } else if (action == GLFW_RELEASE) {
-            switch (key) {
-                case GLFW_KEY_W -> camera.cancelF();
-                case GLFW_KEY_S -> camera.cancelB();
-                case GLFW_KEY_A -> camera.cancelL();
-                case GLFW_KEY_D -> camera.cancelR();
-                case GLFW_KEY_SPACE -> camera.cancelFly();
-                case GLFW_KEY_LEFT_SHIFT -> camera.cancelLand();
-            }
+            if (releaseKeymap.get(key) != null) // Defined keybindings
+                releaseKeymap.get(key).run();
         }
     }
 }
