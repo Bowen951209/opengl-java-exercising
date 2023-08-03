f9c941396135bb4f6b1b8caaa5b96b22b467bc06
bowen51209 <bowen51209@gmail.com>
2023-08-03 20:53:57 +0800

    add TODOs

--- src/main/java/chapter15/Main.java
+++ src/main/java/chapter15/Main.java
@@ -2,4 +2,7 @@ package chapter15;
 
 /*This chapter will simulate water*/
 public class Main {
+    // TODO: 2023/8/3 Skybox
+    // TODO: 2023/8/3 pool floor with ADS lighting (include checkerboard texture)
+    // TODO: 2023/8/3 plane with ADS lighting (for water, no texture) if camera is higher than plane, render up face, else render bottom face.
 }
