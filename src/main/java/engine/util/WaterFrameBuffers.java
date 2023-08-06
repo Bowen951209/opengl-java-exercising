f0711c69212accd6735a0141ef08b4cb6692e902
bowen51209 <bowen51209@gmail.com>
2023-08-06 08:34:32 +0800

    add WaterFrameBuffers class

---
+++ src/main/java/engine/util/WaterFrameBuffers.java
@@ -0,0 +1,62 @@
+package engine.util;
+import java.nio.ByteBuffer;
+
+import static org.lwjgl.opengl.GL43.*;
+public class WaterFrameBuffers {
+    private final int reflectionFrameBuffer, refractionFrameBuffer;
+
+    public int getReflectionFrameBuffer() {
+        return reflectionFrameBuffer;
+    }
+
+    public int getRefractionFrameBuffer() {
+        return refractionFrameBuffer;
+    }
+
+    public WaterFrameBuffers(int textureWidth, int textureHeight) {
+        this.reflectionFrameBuffer = generateFramebuffer();
+        int reflectionImageTexture = createImageTextureAttachment(textureWidth, textureHeight);
+        int reflectionDepthTexture = createDepthTextureAttachment(textureWidth, textureHeight);
+
+
+        this.refractionFrameBuffer = generateFramebuffer();
+        int refractionImageTexture = createImageTextureAttachment(textureWidth, textureHeight);
+        int refractionDepthTexture = createDepthTextureAttachment(textureWidth, textureHeight);
+    }
+
+    private static int generateFramebuffer() {
+        int framebufferID = glGenFramebuffers();
+        glBindFramebuffer(GL_FRAMEBUFFER, framebufferID);
+        //specify frame buffer to render to color attachment 0
+        glDrawBuffer(GL_COLOR_ATTACHMENT0);
+        return framebufferID;
+    }
+
+    private static int createImageTextureAttachment(int width, int height) {
+        int textureID = glGenTextures();
+        glBindTexture(GL_TEXTURE_2D, textureID);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0,
+                GL_RGB, GL_UNSIGNED_BYTE, (ByteBuffer) null);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+        // get data from current frame buffer's color attachment 0 and putt to texture.
+        glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, textureID, 0);
+
+        return textureID;
+    }
+
+    private static int createDepthTextureAttachment(int width, int height) {
+        int textureID = glGenTextures();
+        glBindTexture(GL_TEXTURE_2D, textureID);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT32, width, height, 0,
+                GL_DEPTH_COMPONENT, GL_FLOAT, (ByteBuffer) null);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+        // get data from current frame buffer's color attachment 0 and putt to texture.
+        glFramebufferTexture(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, textureID, 0);
+
+        return textureID;
+    }
+}
