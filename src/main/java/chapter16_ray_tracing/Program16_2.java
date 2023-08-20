6352d33dc76ac5a56d60e630e1439f274720f41f
bowen51209 <bowen51209@gmail.com>
2023-08-20 11:48:46 +0800

    Add Program16_2.

---
+++ src/main/java/chapter16_ray_tracing/Program16_2.java
@@ -0,0 +1,16 @@
+package chapter16_ray_tracing;
+
+import engine.App;
+import engine.sceneComponents.models.Model;
+
+public class Program16_2 extends App {
+    @Override
+    protected void drawScene() {
+
+    }
+
+    @Override
+    protected void destroy() {
+
+    }
+}
