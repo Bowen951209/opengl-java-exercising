3520fc5e913fab7719c2ae53eec5cd6ceb8fc42f
bowen51209 <bowen51209@gmail.com>
2023-09-04 07:21:04 +0800

    Move code

--- assets/shaders/ch16/16_2/compute/compute.glsl
+++ assets/shaders/ch16/16_2/compute/compute.glsl
@@ -46,7 +46,7 @@ const float DEG_TO_RAD = PI / 180.0;
 
 // Defining Models
 // Plane
-vec3 plane_pos = vec3(0, -2.5, -2.0); //0,-1.2,-2
+vec3 plane_pos = vec3(0, -2.5, -2.0);//0,-1.2,-2
 float plane_width = 12.0;
 float plane_depth = 12.0;
 float plane_xrot = DEG_TO_RAD * 0.0;
@@ -85,30 +85,30 @@ const vec4 light_diffuse = vec4(.7, .7, .7, 1.0);
 const vec4 light_specular = vec4(1.0, 1.0, 1.0, 1.0);
 
 struct Stack_Element
-{	int type;		// The type of ray ( 1 = reflected, 2 = refracted )
-    int depth;		// The depth of the recursive raytrace
-    int phase;	// Keeps track of what phase each recursive call is at (each call is broken down into five phases)
-    vec3 phong_color;		// Contains the Phong ADS model color
-    vec3 reflected_color;	// Contains the reflected color
-    vec3 refracted_color;	// Contains the refracted color
-    vec3 final_color;		// Contains the final mixed output of the recursive raytrace call
-    Ray ray;				// The ray for this raytrace invocation
-    Collision collision;	// The collision for this raytrace invocation. Contains null_collision until phase 1
+{ int type;// The type of ray ( 1 = reflected, 2 = refracted )
+    int depth;// The depth of the recursive raytrace
+    int phase;// Keeps track of what phase each recursive call is at (each call is broken down into five phases)
+    vec3 phong_color;// Contains the Phong ADS model color
+    vec3 reflected_color;// Contains the reflected color
+    vec3 refracted_color;// Contains the refracted color
+    vec3 final_color;// Contains the final mixed output of the recursive raytrace call
+    Ray ray;// The ray for this raytrace invocation
+    Collision collision;// The collision for this raytrace invocation. Contains null_collision until phase 1
 };
 
 const int RAY_TYPE_REFLECTION = 1;
 const int RAY_TYPE_REFRACTION = 2;
 
-Ray null_ray = {vec3(0.0), vec3(0.0)};
+Ray null_ray = { vec3(0.0), vec3(0.0) };
 Collision null_collision = { -1.0, vec3(0.0), vec3(0.0), false, -1, vec2(0.0, 0.0), -1 };
-Stack_Element null_stack_element = {0,-1,-1,vec3(0),vec3(0),vec3(0),vec3(0),null_ray,null_collision};
+Stack_Element null_stack_element = { 0, -1, -1, vec3(0), vec3(0), vec3(0), vec3(0), null_ray, null_collision };
 
 const int stack_size = 100;
 Stack_Element stack[stack_size];
 const int max_depth = 4;
 
-int stack_pointer = -1;			// Points to the top of the stack (-1 if empty)
-Stack_Element popped_stack_element;	// Holds the last popped element from the stack
+int stack_pointer = -1;// Points to the top of the stack (-1 if empty)
+Stack_Element popped_stack_element;// Holds the last popped element from the stack
 
 mat4 buildTranslate(vec3 position){
     return mat4(
@@ -150,48 +150,48 @@ mat4 buildRotation(float xRad, float yRad, float zRad) {
 
 Collision intersect_plane_object(Ray r) {
     // Compute the planes's local-space to world-space transform matrices, and their inverse
-	mat4 local_to_worldT = buildTranslate(plane_pos);
-	mat4 local_to_worldR = buildRotateY(plane_yrot) * buildRotateX(plane_xrot) * buildRotateZ(plane_zrot);
-	mat4 local_to_worldTR = local_to_worldT * local_to_worldR;
-	mat4 world_to_localTR = inverse(local_to_worldTR);
-	mat4 world_to_localR = inverse(local_to_worldR);
+    mat4 local_to_worldT = buildTranslate(plane_pos);
+    mat4 local_to_worldR = buildRotateY(plane_yrot) * buildRotateX(plane_xrot) * buildRotateZ(plane_zrot);
+    mat4 local_to_worldTR = local_to_worldT * local_to_worldR;
+    mat4 world_to_localTR = inverse(local_to_worldTR);
+    mat4 world_to_localR = inverse(local_to_worldR);
 
-	// Convert the world-space ray to the planes's local space:
-	vec3 ray_start = (world_to_localTR * vec4(r.start,1.0)).xyz;
-	vec3 ray_dir = (world_to_localR * vec4(r.dir,1.0)).xyz;
-	
-	Collision c;
-	c.isInside = false;  // there is no "inside" of a plane
-	
-	// compute intersection point of ray with plane
-	c.t = dot((vec3(0,0,0) - ray_start),vec3(0,1,0)) / dot(ray_dir, vec3(0,1,0));
-	
-	// Calculate the world-position of the intersection:
-	c.p = r.start + c.t * r.dir;
-	
-	// Calculate the position of the intersection in plane space:
-	vec3 intersectPoint = ray_start + c.t * ray_dir;
-	
-	// If the ray didn't intersect the plane object, return a negative t value
-	if ((abs(intersectPoint.x) > (plane_width/2.0)) || (abs(intersectPoint.z) > (plane_depth/2.0)))
-	{	c.t = -1.0;
-		return c;
-	}
+    // Convert the world-space ray to the planes's local space:
+    vec3 ray_start = (world_to_localTR * vec4(r.start, 1.0)).xyz;
+    vec3 ray_dir = (world_to_localR * vec4(r.dir, 1.0)).xyz;
 
-	// Create the collision normal
-	c.n = vec3(0.0, 1.0, 0.0);
+    Collision c;
+    c.isInside = false;// there is no "inside" of a plane
 
-	// If we hit the plane from the negative axis, invert the normal
-	if(ray_dir.y > 0.0) c.n *= -1.0;
-	
-	// now convert the normal back into world space
-	c.n = transpose(inverse(mat3(local_to_worldR))) * c.n;
+    // compute intersection point of ray with plane
+    c.t = dot((vec3(0, 0, 0) - ray_start), vec3(0, 1, 0)) / dot(ray_dir, vec3(0, 1, 0));
 
-	// Compute texture coordinates
-	float maxDimension = max(plane_width, plane_depth);
-	c.tc.x = (intersectPoint.x + plane_width/2.0)/maxDimension;
-	c.tc.y = (intersectPoint.z + plane_depth/2.0)/maxDimension;
-	return c;
+    // Calculate the world-position of the intersection:
+    c.p = r.start + c.t * r.dir;
+
+    // Calculate the position of the intersection in plane space:
+    vec3 intersectPoint = ray_start + c.t * ray_dir;
+
+    // If the ray didn't intersect the plane object, return a negative t value
+    if ((abs(intersectPoint.x) > (plane_width/2.0)) || (abs(intersectPoint.z) > (plane_depth/2.0)))
+    { c.t = -1.0;
+        return c;
+    }
+
+    // Create the collision normal
+    c.n = vec3(0.0, 1.0, 0.0);
+
+    // If we hit the plane from the negative axis, invert the normal
+    if (ray_dir.y > 0.0) c.n *= -1.0;
+
+    // now convert the normal back into world space
+    c.n = transpose(inverse(mat3(local_to_worldR))) * c.n;
+
+    // Compute texture coordinates
+    float maxDimension = max(plane_width, plane_depth);
+    c.tc.x = (intersectPoint.x + plane_width/2.0)/maxDimension;
+    c.tc.y = (intersectPoint.z + plane_depth/2.0)/maxDimension;
+    return c;
 }
 
 Collision intersect_sky_box_object(Ray r)
@@ -263,17 +263,17 @@ Collision intersect_sky_box_object(Ray r)
     float rayStrikeY = ((c.p).y  - camera_pos_y+ totalHeight/2.0)/maxDimension;
     float rayStrikeZ = ((c.p).z - camera_pos_z + totalDepth/2.0)/maxDimension;
 
-    if (c.face_index == 0) // xn
+    if (c.face_index == 0)// xn
     c.tc = vec2(rayStrikeZ, -rayStrikeY);
-    else if (c.face_index == 1) // xp
+    else if (c.face_index == 1)// xp
     c.tc = vec2(1.0-rayStrikeZ, -rayStrikeY);
-    else if (c.face_index == 2) // yn
+    else if (c.face_index == 2)// yn
     c.tc = vec2(rayStrikeX, -rayStrikeZ);
-    else if (c.face_index == 3) // yp
+    else if (c.face_index == 3)// yp
     c.tc = vec2(rayStrikeX, rayStrikeZ - 1.0);
-    else if (c.face_index == 4) // zn
+    else if (c.face_index == 4)// zn
     c.tc = vec2(1.0-rayStrikeX, -rayStrikeY);
-    else if (c.face_index == 5) // zp
+    else if (c.face_index == 5)// zp
     c.tc = vec2(rayStrikeX, -rayStrikeY);
 
     return c;
@@ -457,12 +457,12 @@ Collision get_closest_collision(Ray ray) {
         closest_collision.object_index = 3;
     }
     if ((cPlane.t > 0) &&
-		((cPlane.t < cSphere.t) || (cSphere.t < 0))
-		&& ((cPlane.t < cBox.t) || (cBox.t < 0))
-		&& ((cPlane.t < cSBox.t) || (cSBox.t < 0)))
-	{	closest_collision = cPlane;
-		closest_collision.object_index = 4;
-	}
+    ((cPlane.t < cSphere.t) || (cSphere.t < 0))
+    && ((cPlane.t < cBox.t) || (cBox.t < 0))
+    && ((cPlane.t < cSBox.t) || (cSBox.t < 0)))
+    { closest_collision = cPlane;
+        closest_collision.object_index = 4;
+    }
 
     return closest_collision;
 }
@@ -500,18 +500,17 @@ vec3 adsLighting(Ray ray, Collision collision) {
 }
 
 vec3 checkerboard(vec2 tc)
-{	float tileScale = 24.0;
-	float tile = mod(floor(tc.x * tileScale) + floor(tc.y * tileScale), 2.0);
-	return tile * vec3(1,1,1);
+{ float tileScale = 24.0;
+    float tile = mod(floor(tc.x * tileScale) + floor(tc.y * tileScale), 2.0);
+    return tile * vec3(1, 1, 1);
 }
-
 
 
 //------------------------------------------------------------------------------
 // Schedules a new raytrace by adding it to the top of the stack
 //------------------------------------------------------------------------------
 void push(Ray r, int depth, int type)
-{	if (stack_pointer >= stack_size-1)  return;
+{ if (stack_pointer >= stack_size-1)  return;
 
     Stack_Element element;
     element = null_stack_element;
@@ -528,7 +527,7 @@ void push(Ray r, int depth, int type)
 // Removes the topmost stack element
 //------------------------------------------------------------------------------
 Stack_Element pop()
-{	// Store the element we're removing in top_stack_element
+{ // Store the element we're removing in top_stack_element
     Stack_Element top_stack_element = stack[stack_pointer];
 
     // Erase the element from the stack
@@ -546,7 +545,7 @@ void process_stack_element(int index)
     // If there is a popped_stack_element that just ran, it holds one of our values
     // Store it and delete it
     if (popped_stack_element != null_stack_element)
-    {	if (popped_stack_element.type == RAY_TYPE_REFLECTION)
+    { if (popped_stack_element.type == RAY_TYPE_REFLECTION)
     stack[index].reflected_color = popped_stack_element.final_color;
     else if (popped_stack_element.type == RAY_TYPE_REFRACTION)
     stack[index].refracted_color = popped_stack_element.final_color;
@@ -558,13 +557,13 @@ void process_stack_element(int index)
 
     // Iterate through the raytrace phases (explained below)
     switch (stack[index].phase)
-    {	//=================================================
+    { //=================================================
         // PHASE 1 - Raytrace Collision Detection
         //=================================================
         case 1:
-        c = get_closest_collision(r);	// Cast ray against the scene, store the collision result
-        if (c.object_index != -1)		// If the ray didn't hit anything, stop.
-        stack[index].collision = c;	// otherwise, store the collision result
+        c = get_closest_collision(r);// Cast ray against the scene, store the collision result
+        if (c.object_index != -1)// If the ray didn't hit anything, stop.
+        stack[index].collision = c;// otherwise, store the collision result
         break;
         //=================================================
         // PHASE 2 - Phong ADS Lighting Computation
@@ -578,15 +577,15 @@ void process_stack_element(int index)
         case 3:
         // Only make recursive raytrace passes if we're not at max depth
         if (stack[index].depth < max_depth)
-        {	// only the sphere and box are reflective
+        { // only the sphere and box are reflective
             if ((c.object_index == 1) || (c.object_index == 2))
-            {	Ray reflected_ray;
+            { Ray reflected_ray;
                 reflected_ray.start = c.p + c.n * 0.001;
                 reflected_ray.dir = reflect(r.dir, c.n);
 
                 // Add a raytrace for that ray to the stack
                 push(reflected_ray, stack[index].depth+1, RAY_TYPE_REFLECTION);
-            }	}
+            } }
         break;
         //=================================================
         // PHASE 4 - Refraction Transparency Pass Computation
@@ -594,9 +593,9 @@ void process_stack_element(int index)
         case 4:
         // Only make recursive raytrace passes if we're not at max depth
         if (stack[index].depth < max_depth)
-        {	// only the sphere is transparent
+        { // only the sphere is transparent
             if (c.object_index == 1)
-            {	Ray refracted_ray;
+            { Ray refracted_ray;
                 refracted_ray.start = c.p - c.n * 0.001;
                 float refraction_ratio = 0.66667;
                 if (c.isInside) refraction_ratio = 1.0 / refraction_ratio;
@@ -604,18 +603,18 @@ void process_stack_element(int index)
 
                 // Add a raytrace for that ray to the stack
                 push(refracted_ray, stack[index].depth+1, RAY_TYPE_REFRACTION);
-            }	}
+            } }
         break;
         //=================================================
         // PHASE 5 - Mixing to produce the final color
         //=================================================
         case 5:
         if (c.object_index == 1)
-        {	stack[index].final_color = stack[index].phong_color *
+        { stack[index].final_color = stack[index].phong_color *
         ((0.3 * stack[index].reflected_color) + (2.0 * stack[index].refracted_color));
         }
         if (c.object_index == 2)
-        {	stack[index].final_color = stack[index].phong_color *
+        { stack[index].final_color = stack[index].phong_color *
         ((0.5 * stack[index].reflected_color) + (1.0 * (texture(brickTexture, c.tc)).rgb));
         }
         if (c.object_index == 3) stack[index].final_color = stack[index].phong_color * rbox_color;
@@ -627,7 +626,7 @@ void process_stack_element(int index)
         case 6: { popped_stack_element = pop(); return; }
     }
     stack[index].phase++;
-    return;	// Only process one phase per process_stack_element() invocation
+    return;// Only process one phase per process_stack_element() invocation
 }
 
 vec3 raytrace(Ray r){
@@ -636,8 +635,8 @@ vec3 raytrace(Ray r){
 
     // Process the stack until it's empty
     while (stack_pointer >= 0)
-    {	int element_index = stack_pointer;		// Peek at the topmost stack element
-        process_stack_element(element_index);	// Process this stack element
+    { int element_index = stack_pointer;// Peek at the topmost stack element
+        process_stack_element(element_index);// Process this stack element
     }
 
     // Return the final_color value of the last-popped stack element
@@ -655,7 +654,7 @@ const int STATE_DO_DRAW = 1;
 const int STATE_DRAWN = 2;
 
 bool shouldRender(uint index) {
-    if(pixelList[index] == STATE_DO_DRAW) {
+    if (pixelList[index] == STATE_DO_DRAW) {
         return true;
     } else {
         return false;
@@ -667,9 +666,9 @@ void main() {
     ivec2 pixel = ivec2(gl_GlobalInvocationID.xy);
     uint pixelIndex = gl_GlobalInvocationID.x + gl_GlobalInvocationID.y * width;
 
-    calcSkyboxCorners();
 
-    if(shouldRender(pixelIndex)){
+    if (shouldRender(pixelIndex)){
+        calcSkyboxCorners();
         Ray world_ray;
         uint rayInfoIndex = pixelIndex * 3;
 
