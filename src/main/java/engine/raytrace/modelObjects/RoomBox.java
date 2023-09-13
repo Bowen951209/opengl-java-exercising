71716e2acbc157335067b4b76809bf633cfebe9a
bowen51209 <bowen51209@gmail.com>
2023-09-13 20:49:44 +0800

    Remove unused

--- src/main/java/engine/raytrace/modelObjects/RoomBox.java
+++ src/main/java/engine/raytrace/modelObjects/RoomBox.java
@@ -1,7 +1,5 @@
 package engine.raytrace.modelObjects;
 
-import org.joml.Vector3f;
-
 public class RoomBox extends ModelObject{
     public RoomBox(float[] ambient, float[] diffuse, float[] specular, float shininess,
                       boolean hasLighting) {
--- src/main/java/engine/raytrace/modelObjects/Sphere.java
+++ src/main/java/engine/raytrace/modelObjects/Sphere.java
@@ -1,7 +1,5 @@
 package engine.raytrace.modelObjects;
 
-import org.joml.Vector3f;
-
 public class Sphere extends ModelObject {
     public float getRadius() {
         return radius;
