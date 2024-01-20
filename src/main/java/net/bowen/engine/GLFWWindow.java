052f34e54c0632be9cd4598daab61d2b7010f21a
bowen951209 <bowen951209@gmail.com>
2024-01-20 08:45:28 +0800

    Clean up GLFWWindow code.

--- src/main/java/net/bowen/engine/GLFWWindow.java
+++ src/main/java/net/bowen/engine/GLFWWindow.java
@@ -5,16 +5,21 @@ import org.lwjgl.BufferUtils;
 import org.lwjgl.glfw.GLFW;
 import org.lwjgl.glfw.GLFWErrorCallback;
 import org.lwjgl.opengl.GL;
-import static org.lwjgl.opengl.GL43.*;
 import org.lwjgl.system.MemoryUtil;
-
 
 import java.nio.IntBuffer;
 
 import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL43.glClearColor;
+import static org.lwjgl.opengl.GL43.glViewport;
 
 public class GLFWWindow {
+    private static final int MAJOR_VERSION = 4;
+    private static final int MINOR_VERSION = 3;
 
+    private static Color clearColor = new Color(0f, 0f, 0f, 0f);
+
+    private final long windowHandle;
     private final int initWidth, initHeight;
 
     public int getInitWidth() {
@@ -24,6 +29,7 @@ public class GLFWWindow {
     public int getInitHeight() {
         return initHeight;
     }
+
     public int getCurrentWidth() {
         int[] width = new int[1];
         glfwGetWindowSize(getID(), width, null);
@@ -36,20 +42,15 @@ public class GLFWWindow {
         return height[0];
     }
 
-    private final long windowHandle;
+
     public long getID() {
         return windowHandle;
     }
-    private static final int MAJOR_VERSION = 4;
-    private static final int MINOR_VERSION = 3;
 
-
-    private static Color clearColor = new Color(0f, 0f,0f, 0f);
     public void setClearColor(Color clearColor) {
         GLFWWindow.clearColor = clearColor;
         glClearColor(clearColor.getR(), clearColor.getG(), clearColor.getB(), clearColor.getA());
     }
-
 
 
     // NOTE: 因為Gradle 不知道出了什麼問題，中文會顯示亂碼，以前還可以的，所以現在我都只打印英文。
@@ -68,7 +69,7 @@ public class GLFWWindow {
         GLFW.glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, MINOR_VERSION);
         GLFW.glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-        windowHandle = glfwCreateWindow(glfwWidth,glfwHeight, title, MemoryUtil.NULL, MemoryUtil.NULL);
+        windowHandle = glfwCreateWindow(glfwWidth, glfwHeight, title, MemoryUtil.NULL, MemoryUtil.NULL);
         if (windowHandle == MemoryUtil.NULL) {
             System.out.println("Failed to create GLFW Window.");
             glfwTerminate();
